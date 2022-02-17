0bb43093db6e81be9b019673fdb640550f661922
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-02-17 17:28:44 -0600

    payment-wip4

--- react-redux-app/src/components/Customer/Customer.jsx
+++ react-redux-app/src/components/Customer/Customer.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import validator from "validator";
+
 import {
   InputLabel,
   Select,
@@ -9,28 +11,192 @@ import {
   TextField,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import useStyles from "./styles";
 
 const Customer = ({ changeStep }) => {
+  const classes = useStyles();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isError, setIsError] = useState({
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+  });
+
+  const handleFormControlChangeEvent = (event) => {
+    if (event.target.name === "firstName") {
+      setFirstName(event.target.value);
+      if (event.target.value === "") {
+        setIsError({
+          ...isError,
+          firstName: "First Name Is Required!",
+        });
+      } else {
+        setIsError({
+          ...isError,
+          firstName: "",
+        });
+      }
+    } else if (event.target.name === "lastName") {
+      setLastName(event.target.value);
+      if (event.target.value === "") {
+        setIsError({
+          ...isError,
+          lastName: "Last Name Is Required!",
+        });
+      } else {
+        setIsError({
+          ...isError,
+          lastName: "",
+        });
+      }
+    } else if (event.target.name === "email") {
+      setEmail(event.target.value);
+      if (event.target.value === "") {
+        setIsError({
+          ...isError,
+          email: "Email Is Required!",
+        });
+      } else if (validator.isEmail(event.target.value)) {
+        console.log("email ok...");
+        setIsError({
+          ...isError,
+          email: "",
+        });
+      } else {
+        setIsError({
+          ...isError,
+          email: "Invalid Email!",
+        });
+      }
+    }
+  };
+
+  const formValid = (isError) => {
+    let isValid = false;
+    var BreakException = {};
+    try {
+      Object.values(isError).forEach((val) => {
+        console.log("checking... " + val);
+        if (val.length > 0) {
+          isValid = false;
+          throw BreakException;
+        } else {
+          isValid = true;
+        }
+      });
+    } catch (e) {
+      return isValid;
+    }
+    return isValid;
+  };
+
   const handleSubmit = (evt) => {
-    evt.preventDefault();
+    if (formValid(isError)) {
+    } else {
+      console.log("Form is invalid!");
+      if (firstName === "")
+        setIsError({
+          ...isError,
+          firstName: "First Name Is Required!",
+        });
+      if (lastName === "")
+        setIsError({
+          ...isError,
+          lastName: "Last Name Is Required!",
+        });
+      if (email === "")
+        setIsError({
+          ...isError,
+          email: "Email Is Required!",
+        });
+      if (!validator.isEmail(email)) {
+        setIsError({
+          ...isError,
+          email: "Invalid Email!",
+        });
+      }
+      return;
+    }
+
     console.log(
       "Customer Details Submitted! Going to Next Step of Shipping Details!"
     );
 
     // store customer details to redux store
+    let customerDetailsData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
     // and go to next step
     changeStep();
   };
 
   return (
     <div>
-      <h1>Customer Info Form!</h1>
-      <form onSubmit={handleSubmit} noValidate>
+      <Typography variant="h6" gutterBottom>
+        Customer Details
+      </Typography>
+
+      <form noValidate>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="firstName"
+              fullWidth
+              label="First Name"
+              value={firstName}
+              onChange={(e) => handleFormControlChangeEvent(e)}
+            />
+            <span className={classes.controlInvalid}>
+              {isError.firstName && (
+                <span className="invalid-feedback">{isError.firstName}</span>
+              )}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="lastName"
+              fullWidth
+              label="Last Name"
+              value={lastName}
+              onChange={(e) => handleFormControlChangeEvent(e)}
+            />
+            <span className={classes.controlInvalid}>
+              {isError.lastName && (
+                <span className="invalid-feedback">{isError.lastName}</span>
+              )}
+            </span>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="email"
+              fullWidth
+              label="Email"
+              value={email}
+              onChange={(e) => handleFormControlChangeEvent(e)}
+            />
+            <span className={classes.controlInvalid}>
+              {isError.email && (
+                <span className="invalid-feedback">{isError.email}</span>
+              )}
+            </span>
+          </Grid>
+        </Grid>
+        <br />
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <Button component={Link} variant="outlined" to="/cart">
             Back to Cart
           </Button>
-          <Button type="submit" variant="contained" color="primary">
+          <Button
+            onClick={(e) => handleSubmit(e)}
+            type="button"
+            variant="contained"
+            color="primary"
+          >
             Next
           </Button>
         </div>
--- react-redux-app/src/components/Customer/styles.js
+++ react-redux-app/src/components/Customer/styles.js
@@ -4,4 +4,7 @@ export default makeStyles((theme) => ({
   main: {
     marginTop: "100px",
   },
+  controlInvalid: {
+    color: "red",
+  },
 }));
