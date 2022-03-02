abfcf688b3dae1bb8200df7b8e6526b9f03da5e9
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-03-02 13:07:45 -0600

    admin-area-wip2

--- react-redux-app/src/components/Admin-ProductSales/AdminProductSales.jsx
+++ react-redux-app/src/components/Admin-ProductSales/AdminProductSales.jsx
@@ -16,7 +16,27 @@ const AdminProductSales = () => {
   return (
     <div className={classes.main}>
       <Container maxWidth="md">
-        <h1>Admin Area [ Product Sales ]</h1>      
+        <h1>Admin Area [ Product Sales ]</h1>
+        <p></p>
+        <div>
+          <ul>
+            <li>Select Product from list</li>
+            <li>
+              Display Bar chart of selected product for current year's total
+              sales
+            </li>
+            <li>Select Year from list</li>
+            <li>
+              Display Bar chart of selected product for selected year's total
+              sales
+            </li>
+            <li>Select 5 years Sales from list</li>
+            <li>
+              Display Bar chart of selected product for 5 years' total
+              sales
+            </li>
+          </ul>
+        </div>
       </Container>
     </div>
   );
