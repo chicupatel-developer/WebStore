13f684470bf81da4064f8363d6b27ab86185e9b0
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-11-01 11:42:16 -0500

    webapi-startup-ok

---
+++ WebAPI/WebStoreAPI/Controllers/WebStoreController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebStoreController : ControllerBase
+    {     
+        private readonly ILogger<WebStoreController> _logger;
+
+        public WebStoreController(ILogger<WebStoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome To WebStore App !" + "\n" + "This App requires Fake-Store-API(https://fakestoreapi.com/) to access Products !";
+        }
+    }
+}
